54c075388727cd5c27cb7656d235d91cb50e38ef
christopher@zarlengo.net <christopher@zarlengo.net>
2020-10-03 21:21:40 -0700

    eslint update

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,7 +3,6 @@ const db = require("../models");
 const passport = require("../config/passport");
 const Op = db.Sequelize.Op;
 
-
 module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
