c42022ef9f09f645016bac27a522fc7334c89f57
Zarlengo <christopher@zarlengo.net>
2020-10-05 16:24:40 -0700

    placeholder

--- public/js/solar.js
+++ public/js/solar.js
@@ -5,6 +5,7 @@ fetch(geoIPLookupURL)
   .then(result => {
     const locationString = `${result.city}, ${result.region} ${result.country_code}`;
     document.querySelector("#location").textContent = locationString;
+    getUsage(result.region);
   });
 
 // Adds a listener to the search button to allow loading based on GPS coordinates
@@ -102,3 +103,63 @@ document.querySelector(".location-icon").addEventListener(
   },
   false
 );
+
+function getUsage(stateCode) {
+  const stateList = {
+    AL: 1211,
+    AK: 632,
+    AZ: 1049,
+    AR: 1133,
+    CA: 557,
+    CO: 723,
+    CT: 752,
+    DE: 944,
+    DC: 720,
+    FL: 1078,
+    GA: 1088,
+    HI: 515,
+    ID: 1055,
+    IL: 755,
+    IN: 1005,
+    IA: 908,
+    KS: 926,
+    KY: 1154,
+    LA: 1273,
+    ME: 551,
+    MD: 1031,
+    MA: 638,
+    MI: 665,
+    MN: 817,
+    MS: 1220,
+    MO: 1086,
+    MT: 860,
+    NE: 1034,
+    NV: 924,
+    NH: 629,
+    NJ: 687,
+    NM: 655,
+    NY: 602,
+    NC: 1098,
+    ND: 1205,
+    OH: 892,
+    OK: 1142,
+    OR: 976,
+    PA: 857,
+    RI: 602,
+    SC: 1124,
+    SD: 1055,
+    TN: 1245,
+    TX: 1174,
+    UT: 798,
+    VY: 569,
+    VA: 1156,
+    WA: 1041,
+    WV: 1118,
+    WI: 703,
+    WY: 894
+  };
+  if (stateCode in stateList) {
+    return stateList[stateCode];
+  }
+  return 909;
+}
