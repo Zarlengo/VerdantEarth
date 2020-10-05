df0380a3fde5ff9066fb116cf41f8d06474fc468
christopher@zarlengo.net <christopher@zarlengo.net>
2020-10-04 21:54:21 -0700

    Put in pictures

--- public/js/product.js
+++ public/js/product.js
@@ -1,29 +1,42 @@
 const tagQuery = event => {
-  const tag = event.target.getAttribute("data-id");
+  const tag = event.currentTarget.getAttribute("data-id");
+  console.log(`/api/products/${tag}`);
   fetch(`/api/products/${tag}`)
     .then(response => {
       return response.json();
     })
     .then(json => {
+      console.log(json);
+      const prodContainer = document.querySelector(".product-container");
+      prodContainer.innerHTML = "";
       json.forEach(listing => {
-        console.log(listing.listingId);
         fetch(`/api/products/${listing.listingId}/image`)
           .then(response => {
             return response.json();
           })
           .then(imageURL => {
-            const title = listing.title;
+            const newDiv = document.createElement("div");
+            let title = listing.title;
+            if (title.length > 150) {
+              title = title.substring(0, 150) + "...";
+            }
             const titleP = document.createElement("p");
             titleP.textContent = title;
+            newDiv.appendChild(titleP);
 
-            const description = listing.description;
+            let description = listing.description;
+            if (description.length > 150) {
+              description = description.substring(0, 150) + "...";
+            }
             const descriptionP = document.createElement("p");
             descriptionP.textContent = description;
+            newDiv.appendChild(descriptionP);
 
             const imageTag = document.createElement("img");
             imageTag.src = imageURL;
+            newDiv.appendChild(imageTag);
 
-            // NEED TO PUT CODE TO INSERT INTO HTML HERE
+            prodContainer.appendChild(newDiv);
           })
           .catch(error => console.log(error));
       });
