841fe2792db60ab19c2a9f64c2504fb4a0b84684
Zarlengo <christopher@zarlengo.net>
2020-10-05 12:23:58 -0700

    update signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,30 +3,33 @@ $(document).ready(() => {
   const signUpForm = $("form.signup");
   const emailInput = $("input#email-input");
   const passwordInput = $("input#password-input");
+  const optinInput = $("input#optin-input");
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", event => {
     event.preventDefault();
     const userData = {
       email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
+      password: passwordInput.val().trim(),
+      emailOptIn: optinInput.checked
     };
 
     if (!userData.email || !userData.password) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
+    signUpUser(userData.email, userData.password, userData.emailOptIn);
     emailInput.val("");
     passwordInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
+  function signUpUser(email, password, emailOptIn) {
     $.post("/api/signup", {
       email: email,
-      password: password
+      password: password,
+      emailOptIn: emailOptIn
     })
       .then(() => {
         window.location.replace("/profile");
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -23,7 +23,8 @@ module.exports = function(app) {
   app.post("/api/signup", (req, res) => {
     db.User.create({
       email: req.body.email,
-      password: req.body.password
+      password: req.body.password,
+      emailOptIn: req.body.emailOptIn
     })
       .then(() => {
         res.redirect(307, "/api/login");
